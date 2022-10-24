9bffefedc92d33eda0f1e9257a54e0974d2c566e
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-10-24 21:48:41 +0300

    Iterators/generators

---
+++ lesson_04/1.py
@@ -0,0 +1,22 @@
+names = ["John", "Mary", "Carl"]
+
+
+def get_name():
+    return "Dima"
+
+
+# def get_name_gen():
+#     for name in names:
+#         try:
+#             do_something(name)
+#         except Exception as error:
+#             q = 22
+
+
+name = get_name()
+print(name)
+
+# gen = get_name_gen()
+
+# for d in gen:
+#     print(d)
